124ef305d5134e3b510d3a25c0155ae75760cdff
Sudhanshu Shekhar <sudhanshuvshekhar@gmail.com>
2021-12-11 17:55:17 +0530

    added controlled editors

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
+
+function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
+  return (
+    <>
+      <div className="pane top-pane grid grid-cols-3 gap-2 px-4 mt-6">
+        <Editor 
+          language= "xml"
+          displayName= "HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+          language= "css"
+          displayName= "CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor 
+          language= "javascript"
+          displayName= "Javascript"
+          value={js}
+          onChange={setJs}
+        />
+        
+      </div>
+      <div className="bottom-pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
 
-const Editor = () => {
-    return <p>Haloooooo Mfsssss</p>
+const Editor = (props) => {
+    const{
+        language,
+        displayName,
+        value,
+        onChange
+    } = props;
+
+    const handleChange=(editor, data, value)=>{
+        onChange(value);
+    };
+
+    return (
+        <div className="editor-container">
+            <div className="editor-title rounded-sm">
+                {displayName}
+                <button className=" border-2 ">O/C</button>
+            </div>
+            <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper backdrop-blur-sm"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers:true,
+                    theme: 'material'
+                }}
+            />
+        </div>
+    );
 };
 
 export default Editor;
+
